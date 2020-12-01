ea062020a67fb8f5b2a0f60f4f7c967d1c2317a2
alex <sasichkamega@gmail.com>
2020-12-01 15:05:47 +0300

    удаление закомментированного кода

--- src/core/src/SystemRegistry.cpp
+++ src/core/src/SystemRegistry.cpp
@@ -16,49 +16,6 @@ SystemRegistry::SystemRegistry(std::shared_ptr<Logger> logger,
         registry(registry),
         process(process)
 {
-  /*logger->info("Инициализация систем");
-  const std::vector<SystemDefine*>& defined_systems = SystemDefine::get_defined_systems();
-  std::vector<std::pair<int,std::unique_ptr<System>>> unsorted_systems;
-  std::unordered_map<const System*, std::string> system_names;
-
-  for (SystemDefine* system_define : defined_systems) {
-    logger->trace("Инициализация {}", system_define->name);
-    try {
-      std::unique_ptr<System> system = system_define->build_func(*locator);
-      if (!system) {
-        logger->error(
-          "Не удалось создать систему {}. Функция инициализации вернула nullptr", system_define->name
-        );
-        continue;
-      }
-      system_names.insert({system.get(), system_define->name});
-      System::Settings settings;
-      system->setup(settings);
-      unsorted_systems.emplace_back(settings.priority,std::move(system));
-    }
-    catch(const std::exception& e) {
-      logger->error("Не удалось инициализировать систему {}: {}", system_define->name, e.what());
-    }
-  } //for loop end
-  logger->trace("Сортировка систем");
-  std::sort(unsorted_systems.begin(), unsorted_systems.end(), [](const auto& a, const auto& b ){
-    return a.first < b.first;
-  });
-  for (auto& pair : unsorted_systems) {
-    systems.push_back(std::move(pair.second));
-  }
-  logger->info("Зарегистрировано систем: {}", systems.size());
-
-  std::string priority_report = "Порядок вызова систем: ";
-  for (const auto& system : systems) {
-    priority_report+= "\n  ";
-    priority_report+= system_names[system.get()];
-  }
-  logger->debug("{}", std::move(priority_report));
-
-  for (const auto& system : systems) {
-    system->init(*registry);
-  }*/
   logger->info("Инициализация систем");
   std::unordered_map<std::string, int> priorities = load_systems_priorities();
   for (SystemDefine* system_define : SystemDefine::get_defined_systems()) {
