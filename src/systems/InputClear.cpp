ae3080a049158f1c49eaf6d45142842bc2328da8
alex <sasichkamega@gmail.com>
2020-12-03 13:28:08 +0300

    Фикс обновления component::ListenKeyboard каждый кадр при очистке

--- src/systems/InputClear.cpp
+++ src/systems/InputClear.cpp
@@ -1,13 +1,26 @@
 #include <core/define_system.hpp>
 #include <components/Input.hpp>
+
 namespace {
 
 class InputClear : public core::System {
   std::vector<sf::Keyboard::Key> to_remove;
 public:
   void update(entt::registry& registry) override {
+    //С этим говнокодом определенно надо что-то сделать
     auto view = registry.view<component::ListenKeyboard>();
     for (entt::entity entity : view) {
+      const auto& const_keyboard = view.get<component::ListenKeyboard>(entity);
+      bool need_patch = false;
+      for (auto& [key, state] : const_keyboard.keys) {
+        if (state.up || state.down) {
+          need_patch = true;
+          break;
+        }
+      }
+      if (!need_patch) {
+        continue;
+      }
       registry.patch<component::ListenKeyboard>(entity, [this](auto& keyboard) {
         to_remove.clear();
         for (auto& [key, state] : keyboard.keys) {
