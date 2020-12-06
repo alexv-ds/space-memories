8cf38c9b110bccb0a006d8a4cb34bf881c0941bc
alex <sasichkamega@gmail.com>
2020-12-06 22:26:49 +0300

    учитывание delta_time при расчете шага

--- src/systems/WASDRawInputMovable.cpp
+++ src/systems/WASDRawInputMovable.cpp
@@ -1,31 +1,36 @@
 #include <core/define_system.hpp>
+#include <services/Time.hpp>
 #include <components/Input.hpp>
 #include <components/Position.hpp>
 
 namespace {
 
 class WASDRawInputMovable final : public core::System {
-  float speed = 0.1f;
+  std::shared_ptr<service::Time> time;
+  float speed = 6.0f;
+  float step_size;
 public:
+  WASDRawInputMovable(std::shared_ptr<service::Time> time): time(std::move(time)) {}
   void update(entt::registry& registry) override {
+    step_size = speed * time->get_delta();
     auto view = registry.view<component::WASDRawInputMovable, component::Position, component::ListenKeyboard>();
     view.each([&, this](auto entity, const auto& wasd_movable, auto position, const auto& keyboard) {
       if (registry.valid(wasd_movable.input_listener)) {
         bool need_update = false;
         if (keyboard.is_key_pressed(sf::Keyboard::W)) {
-          position.y += speed;
+          position.y += step_size;
           need_update = true;
         }
         if (keyboard.is_key_pressed(sf::Keyboard::A)) {
-          position.x -= speed;
+          position.x -= step_size;
           need_update = true;
         }
         if (keyboard.is_key_pressed(sf::Keyboard::S)) {
-          position.y -= speed;
+          position.y -= step_size;
           need_update = true;
         }
         if (keyboard.is_key_pressed(sf::Keyboard::D)) {
-          position.x += speed;
+          position.x += step_size;
           need_update = true;
         }
         if (need_update) {
@@ -38,7 +43,7 @@ public:
 };
 
 CORE_DEFINE_SYSTEM("system::WASDRawInputMovable", [](core::ServiceLocator& locator) {
-  return std::make_unique<WASDRawInputMovable>();
+  return std::make_unique<WASDRawInputMovable>(locator.get<service::Time>());
 });
 
 }
